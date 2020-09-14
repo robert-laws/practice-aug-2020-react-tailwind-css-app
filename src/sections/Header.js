27a38d52954df79b5221eef96e0c3c0fac9ee7f6
Robert Laws <roblaws@me.com>
2020-09-14 17:13:03 +0300

    update header for responsive display

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../images/checkmark-outline.svg';
 
 export const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <header className='w-full bg-green-800 flex-initial flex flex-col md:flex-row items-center p-3'>
-      <div className='flex flex-col flex-1 md:flex-initial justify-center items-center w-20 h-auto my-3 ml-3'>
+    <header className='w-full bg-green-800 flex-initial flex flex-col md:flex-row p-3'>
+      <div className='flex flex-row flex-1 md:flex-1 justify-between items-center w-full h-auto my-3 ml-3'>
         <Link
           to='/'
           className='mx-4 my-1 font-bold text-lg hover:text-green-300 text-white'
@@ -18,8 +20,25 @@ export const Header = () => {
             }}
           />
         </Link>
+        <div
+          className='px-4 cursor-pointer md:hidden'
+          id='burger'
+          onClick={() => setToggle(!toggle)}
+        >
+          <svg viewBox='0 0 20 20' fill='#FFF' className='menu w-8 h-8'>
+            <path
+              fillRule='evenodd'
+              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </div>
       </div>
-      <div className='flex flex-col md:flex-row flex-1 justify-center md:justify-end items-center'>
+      <div
+        className={`flex flex-col md:flex-row flex-initial justify-center md:justify-end items-center self-center ${
+          toggle ? 'block' : 'hidden'
+        } md:block`}
+      >
         <Link
           to='/'
           className='mx-4 my-1 font-bold text-lg hover:text-green-300 text-white'
